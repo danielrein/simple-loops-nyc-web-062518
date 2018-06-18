361cc57af876a5c7ea620995cab6c9723e9684b0
Daniel Ran <danielr1735@gmail.com>
2018-06-18 20:35:37 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -2,6 +2,12 @@
 
 def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  n = 0
+  loop do
+    puts phrase
+    n += 1
+    break if n = number_of_times
+  end
 
 end
 
